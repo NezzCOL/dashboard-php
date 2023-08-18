52f8fe243b28313161cf9e142c18feccd432bb95
Desarrollo ADSO6 <proyectosenasena0089@gmail.com>
2023-08-18 12:07:51 -0500

    dashboard-php

--- comunes/registro-usuario.php
+++ comunes/registro-usuario.php
@@ -10,6 +10,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="../css/styles.css" rel="stylesheet" />
+        <link rel="stylesheet" href="../Login/styles.css">
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
@@ -117,8 +118,10 @@
                                                     <td><?php echo $datos->telefono ?></td>
                                                     <td><?php echo $datos->Rol?></td>
                                                     <td>
-                                                        <button href="" class="btn btn-small btn-warning"><i class="fa-solid fa-pen-to-square"></i></button>
-                                                        <button href="" class="btn btn-small btn-danger"><i class="fa-solid fa-trash"></i></button>
+                                                    <div class="icon-buttons">
+                                                        <button href="#" class="btn btn-small btn-warning "><i class="fa-solid fa-pen-to-square"></i></button>
+                                                        <button href="#" class="btn btn-small btn-danger"><i class="fa-solid fa-trash"></i></button>
+                                                    </div>
                                                     </td>
                                                 </tr>
                                             <?php
