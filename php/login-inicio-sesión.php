c249cb38b1f390f572a7c0e64271adcabd479200
Desarrollo ADSO6 <proyectosenasena0089@gmail.com>
2023-08-23 11:52:37 -0500

    dashboard-php

--- php/login-inicio-sesión.php
+++ php/login-inicio-sesión.php
@@ -2,12 +2,13 @@
 
 if(!empty($_POST["btn"])){
     
-    $conexion = mysqli_connect("localhost","root","","login");
-    $USUARIO = $_POST['usuario'];
+    include('../Login/db.php')
+
+    $usuario = $_POST['usuario'];
     $contraseña = $_POST['contraseña'];
     $MD5contraseña = md5($contraseña);
 
-    $consulta = "SELECT * FROM asistencias where usuario = '$USUARIO' and contraseña = '$MD5contraseña' ";
+    $consulta = "SELECT * FROM asistencias where usuario = $usuario and contraseña = '$MD5contraseña' ";
     $resultado = mysqli_query($conexion, $consulta);
 
     $filas = mysqli_num_rows($resultado);
